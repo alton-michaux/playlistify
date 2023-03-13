71c9505e1fb5c8da153b9b51b5fd408cb4685e34
Alton Michaux <alton.michaux@yahoo.com>
2023-03-12 23:35:50 -0400

    adding neccesary elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Input from 'components/elements/Input'
 import Nav from 'components/elements/Nav'
 import Main from 'components/elements/Main'
+import TrackWindow from 'components/elements/TrackWindow'
+import MainPlaylist from 'components/elements/MainPlaylist'
+import Metadata from './components/elements/Metadata'
 
 function App() {
   return (
     <div className='container'>
       <Input></Input>
       <Nav></Nav>
-      <Main></Main>
+      <Main>
+        <TrackWindow></TrackWindow>
+        <MainPlaylist></MainPlaylist>
+        <Metadata></Metadata>
+      </Main>
     </div>
   );
 }
