71f0b0bb023329d6a6f132ad58f0a9923d42af8c
Alton Michaux <alton.michaux@yahoo.com>
2023-03-12 23:14:15 -0400

    added main, nav and input components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Input from 'components/elements/Input'
+import Nav from 'components/elements/Nav'
+import Main from 'components/elements/Main'
 
 function App() {
   return (
     <div className='container'>
+      <Input></Input>
+      <Nav></Nav>
+      <Main></Main>
     </div>
   );
 }
