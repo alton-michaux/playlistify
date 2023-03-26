54db776fdc39fc2a8b231f0b3c25610ac76ac96a
alton-michaux <alton.michaux@yahoo.com>
2023-03-26 10:37:16 -0400

    responsive viewer in chrome giving me unreliable feedback, just going to leave basic styling as is for now. May have better luck on desktop

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -1,8 +1,10 @@
+import '../../assets/App.css'
+
 const Footer = () => {
   return (
     <footer
       className="text-center"
-      style={{ margin: "0 auto", height: "8vh", backgroundColor: "#383E47" }}
+      style={{ margin: "0 auto", height: "6vh", backgroundColor: "#383E47", position: "fixed", left: 0, bottom: 0, marginTop: "5vh" }}
     >
       <div
         className="m-3"
