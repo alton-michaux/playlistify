6bb080351e85da50d6a2a4b9c8feaf7a10b1a396
Alton Michaux <alton.michaux@yahoo.com>
2023-03-31 15:52:58 -0400

    working on api data

--- src/components/elements/PlaylistElements/PlaylistArt.js
+++ src/components/elements/PlaylistElements/PlaylistArt.js
@@ -1,14 +1,17 @@
 import Image from 'react-bootstrap/Image'
+import "../../../styles/App.css"
 
-const PlaylistArt = ({image}) => {
+const PlaylistArt = ({ image }) => {
   return (
-    <section>
+    <div
+      className="playlistImageDiv"
+    >
       <Image
         src={image}
         fluid
       >
       </Image>
-    </section>
+    </div>
   )
 }
 
