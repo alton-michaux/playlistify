20714c7fa019cc4da73b263b2366f6cb76adabca
Alton Michaux <alton.michaux@yahoo.com>
2023-04-01 21:27:06 -0400

    still adding images for genres

--- src/components/elements/SelectElements/PlaylistList.js
+++ src/components/elements/SelectElements/PlaylistList.js
@@ -22,10 +22,18 @@ const PlaylistList = ({ playlists, handleTracklistFetch, genre }) => {
 
   const handleBackgroundImg = (genre) => {
     switch (genre) {
-      case "chill":
-        return "https://i.pinimg.com/originals/58/9b/2b/589b2b8ebf2abfe7345747c768b7aa8e.jpg"
+      case "chill", "study":
+        return "https://wallpapercave.com/wp/wp10837598.png"
       case "hip-hop":
-        return "https://i.ytimg.com/vi/rnV-znkfalg/maxresdefault.jpg"
+        return "https://wallpapers.com/images/hd/dark-city-digital-art-6vx1qudd00qlnz17.jpg"
+      case "club", "party":
+        return "https://c1.wallpaperflare.com/preview/581/162/430/smoke-party-people-dark.jpg"
+      case "pop":
+        return "https://media.istockphoto.com/id/1199262104/vector/illuminated-stage-with-scenic-lights-and-smoke-blue-vector-spotlight-with-smoke-volume-light.jpg?s=612x612&w=0&k=20&c=sGterFSNO2hzFRt6NRg_VDLkvur2UIloA5v5pDGFtVg="
+      case "r-n-b":
+        return "https://wallpapers.com/images/hd/dark-heart-on-window-pane-a2fqmtwop8i9narh.jpg"
+      case "sad", "rainy-day":
+        return "https://wallpaperaccess.com/full/777992.jpg"
       default:
         return "https://thumbs.dreamstime.com/b/abstract-black-red-blue-background-vector-illustration-abstract-black-red-blue-background-vector-illustration-beautiful-204073511.jpg"
     }
