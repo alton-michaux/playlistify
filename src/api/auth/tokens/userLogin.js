19db423139c451daed9a63deb91d1dbfc1dfc59d
Alton Michaux <alton.michaux@yahoo.com>
2023-04-22 12:32:19 -0400

    working on getting login call to work

--- src/api/auth/tokens/userLogin.js
+++ src/api/auth/tokens/userLogin.js
@@ -1,21 +1,11 @@
-import utils from "../../../utils/utils"
+import axios from "axios";
 
 async function UserLogin() {
-  // Open the auth popup
-  window.location.href = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=token`
-
-  const authToken = utils.URLToken
-  // console.log('authtoken', authToken)
-
-  try {
-    window.spotifyCallback = async (payload) => {
-      return payload
-    }
-
-    window.spotifyCallback(authToken);
-  } catch (error) {
+  axios.get('/login').then((response) => {
+    return response.json()
+  }).catch((error) => {
     throw new Error(`Error! status: ${error}`);
-  }
+  })
 }
 
 export default UserLogin
